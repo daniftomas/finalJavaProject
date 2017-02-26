915dcafddeaab1c027aa426405c7bddbb924ca99
Altran <Altran@LPT-FA3301.lan>
2017-02-26 15:44:54 +0000

    d.1.3 - opt 1 3 and 4 working

--- finalJavaProject/src/finalJavaProject/Main.java
+++ finalJavaProject/src/finalJavaProject/Main.java
@@ -1,10 +1,16 @@
 package finalJavaProject;
 
+
+import java.awt.Desktop;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+
+
 
 public class Main {
 
@@ -50,7 +56,7 @@ public class Main {
 			break;
 		case 4:
 			System.out.println("cenas4\n");
-			
+			exportClientsListToTXT();
 			break;
 		case 5:
 			System.out.println("cenas5\n");
@@ -279,19 +285,211 @@ public class Main {
 	 
 	 
 	 
-	 
-	 //OPTION 4
-	 public static void exportCustomerOrderList(){
-		 
-		 Customer cliente = new Customer();
-		 
-		 
-	 }
-	 
-	 public static void importListEmployees(){
-		 
-		 
-	 }
+	// OPTION 4
+
+		public static void exportClientsListToTXT() {
+			FileIO.exportToFile(DataBase.getCustomerList());
+			try {
+				Desktop.getDesktop().open(new java.io.File(FileIO.CUSTOMERSTXT));
+			} catch (IOException e) {
+				System.out.println(
+						"Sorry, there was an error while openning the document! Please, check the success in the creation of it.");
+				e.printStackTrace();
+			}
+		}
+
+		public static void importClientsFromBinaryDocument() {
+			String x = "y";
+			System.out.print("Do you need to create customers first? (y/n/x to e(x)it)\n");
+			x = Utilities.readString();
+			if ((x.equals("x"))||(x.equals("X"))) return;
+			List<Customer> customers = new ArrayList<>();
+			if ((x.equals("y")) || (x.equals("Y"))) {
+				do {
+					Customer client = new Customer();
+					String fName;
+					String lName;
+					String phone;
+					String adress1;
+					String adress2;
+					String city;
+					String state;
+					String country;
+					String postalCode;
+					int salesRepEmployeeNumber;
+					double creditLimit;
+					System.out.println("************ Customer creation **************\n");
+					// fisrt name
+					System.out.print("Insert Fist Name: ");
+					do {
+						fName = Utilities.readString();
+						if (fName.length() == 0) {
+							System.out.println("you may not leave the name in blank.");
+						}
+						if (fName.length() > 50) {
+							System.out.println("you can olnly use 50 char");
+						}
+					} while (fName.length() == 0 || fName.length() > 50);
+					client.setFirstName(fName);
+					// last name
+					System.out.print("Insert Last Name: ");
+					do {
+						lName = Utilities.readString();
+						if (lName.length() == 0) {
+							System.out.print("you may not leave the field in blank. Insert last Name: ");
+						}
+						if (lName.length() > 50) {
+							System.out.print("you can only use 50 char. Insert last Name:");
+						}
+
+					} while (lName.length() == 0 || lName.length() > 50);
+
+					client.setLastName(lName);
+					// phone
+					System.out.print("Insert phone: ");
+					phone = Utilities.insertPhone();
+					client.setPhone(phone);
+
+					// adressline1
+					System.out.print("Insert adressLine 1: ");
+					do {
+						adress1 = Utilities.readString();
+						if (adress1.length() == 0) {
+							System.out.print("you may not leave the field in blank. Insert addressLine 1: ");
+						}
+						if (adress1.length() > 50) {
+							System.out.print("you can olnly use 50 char. Insert addressLine 2: ");
+						}
+
+					} while (adress1.length() == 0 || adress1.length() > 50);
+					client.setAddressLine1(adress1);
+					// adressLine2
+					System.out.print("Insert adressLine 2: ");
+					do {
+						adress2 = Utilities.readString();
+						if (adress2.length() == 0) {
+							System.out.print("you may not leave the field in blank. Insert addressLine 2: ");
+						}
+						if (adress2.length() > 50) {
+							System.out.print("you can olnly use 50 char. Insert addressLine 2: ");
+						}
+
+					} while (adress2.length() == 0 || adress2.length() > 50);
+					client.setAddressLine2(adress2);
+					// city5
+					System.out.print("Insert city: ");
+					do {
+						city = Utilities.readString();
+						if (city.length() == 0) {
+							System.out.print("you may not leave the field in blank. Insert city: ");
+						}
+						if (city.length() > 50) {
+							System.out.print("you can olnly use 50 char. Insert city: ");
+						}
+
+					} while (city.length() == 0 || city.length() > 50);
+					client.setCity(city);
+					// state
+					System.out.print("Insert state: ");
+					do {
+						state = Utilities.readString();
+						if (state.length() == 0) {
+							System.out.print("you may not leave the field in blank. Insert state: ");
+						}
+						if (state.length() > 50) {
+							System.out.print("you can olnly use 50 char. Insert state: ");
+						}
+
+					} while (state.length() == 0 || state.length() > 50);
+					client.setState(state);
+					// postalCode
+					System.out.print("Insert postal code: ");
+					boolean passou = false;
+					do {
+						if (passou) {
+							System.out.print("Bad postal code, insert again: ");
+							passou = true;
+						}
+						postalCode = Utilities.readString();
+					} while (!Utilities.validateCodePostal(postalCode));
+					client.setPostalCode(postalCode);
+					// country
+					System.out.print("Insert country: ");
+					country = Utilities.readString();
+					client.setCountry(country);
+					// salesrepEmploeeNumber
+					List<Employee> lu = DataBase.getEmployeesList();
+					String employee;
+					System.out.println("\n******* Employees List *******\n");
+					for (int i = 0; i < lu.size(); i++) {
+						employee = "" + (i + 1);
+						System.out.println(employee + ". " + lu.get(i).toString());
+					}
+					System.out.print("Select the responsible Employeer (number): ");
+					do {
+						salesRepEmployeeNumber = Utilities.readNumber();
+						if (salesRepEmployeeNumber > lu.size()) {
+							System.out.println("There are only " + lu.size() + " employees.");
+						}
+
+					} while (salesRepEmployeeNumber > lu.size());
+					client.setSalesRepEmployeeNumber(salesRepEmployeeNumber - 1);
+					// creditLimit
+					System.out.print("Insert Credit limit: ");
+					creditLimit = Utilities.readDouble();
+					client.setCreditlimit(creditLimit);
+					customers.add(client);
+					System.out.print("Do you want to add another customer?? (Y/N)");
+					x = Utilities.readString();
+				} while ((x.equals("y")) || (x.equals('Y')));
+				if (!customers.isEmpty()) {
+					FileIO.exportCustomersBinary(customers);
+					java.io.File fil = new java.io.File(FileIO.BINARYFILEFINAL);
+					if (fil.isFile()) {
+						List<Customer> ll = FileIO.importCustomersBinary();
+						if (ll.isEmpty()) {
+							System.out.println("Something went wrong!! The list of customers is empty. Try another file!! Press Enter");
+							Utilities.readString();
+						} else if (DataBase.insertCustomersList(ll)) {
+							System.out.println("Action successfull! The Customer(s) was(were) created!! Press Enter");
+							Utilities.readString();
+						} else {
+							System.out.println("An error happened! Try again. Press Enter");
+							Utilities.readString();
+						}
+					} else {
+						System.out.println("An error happened! Try again. Press Enter");
+						Utilities.readString();
+					}
+				}
+			} else {
+				java.io.File fil;
+				System.out.println("Please, give me the path of the binary file! (x to e(x)it)");
+				boolean entrou = false;
+				do {
+					if (entrou) System.out.println("Please, give me the \"true\" path of the binary file! (x to e(x)it)");
+					entrou = true;
+					FileIO.BINARYFILE = Utilities.readString();
+					fil = new java.io.File(FileIO.BINARYFILE);
+					if (fil.isFile()) {
+						List<Customer> ll = FileIO.importCustomersBinary();
+						if (ll.isEmpty()) {
+							System.out.println("Something went wrong!! The list of customers is empty. Try another file!! Press Enter");
+							Utilities.readString();
+						} else if (DataBase.insertCustomersList(ll)) {
+							System.out.println("Action successfull! Press Enter." );
+							Utilities.readString();
+						} else {
+							System.out.println("An error happened! Try again. Press Enter");
+							Utilities.readString();
+						}
+					}
+					if ((x.equals("x"))||(x.equals("X"))) break;
+				} while (!fil.isFile());
+			}
+		}
+		
+		//option 5
 	 
 
 }
